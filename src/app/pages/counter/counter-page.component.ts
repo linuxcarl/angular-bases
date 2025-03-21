02c439d2eccabc6352d19cb441f3b8f03c12efd4
Carlos Ramirez <carlos@extrasistemas.com>
2025-03-20 23:30:51 -0600

    feat: add first signals

--- src/app/pages/counter/counter-page.component.ts
+++ src/app/pages/counter/counter-page.component.ts
@@ -1,15 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter-page.component.html',
+  styles: `
+    button {
+      padding: 6px 10px;
+      width: 65px;
+      margin: 0 10px;
+      border: 1px solid #eee;
+      border-radius: 5px;
+      cursor: pointer;
+    }`,
 })
 export class CounterPageComponent{
   counter = 10;
+  counterSignal = signal<number>(10);
 
   changeValue(value: number ){
     this.counter += value;
+    this.counterSignal.update( counter => counter + value)
   }
   resetValue(){
-    this.counter = 10;
+    this.counter = 0;
+    this.counterSignal.set(0)
   }
 }
