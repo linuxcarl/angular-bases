4cd0df46a494bdca387b49cc339a140c4a517c1d
Carlos Ramirez <carlos@extrasistemas.com>
2025-03-24 21:59:17 -0600

    feat: add flow if and else

--- src/app/pages/dragonball/dragonball.component.ts
+++ src/app/pages/dragonball/dragonball.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 interface Character{
   id: number;
